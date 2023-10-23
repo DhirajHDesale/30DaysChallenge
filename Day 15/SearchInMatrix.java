7e0b27fd069e30a5ec784d2c466a87757488a173
DhirajHDesale <dhirajhanumantpatil@gmail.com>
2023-10-23 17:47:46 +0530

    Search Matrix In Java

---
+++ Day 15/SearchInMatrix.java
@@ -0,0 +1,14 @@
+class Sol
+{
+    public static int matSearch(int mat[][], int N, int M, int X)
+    {
+       
+       for(int i = 0;i<N;i++){
+           for(int j = 0;j<M;j++){
+               if(mat[i][j]==X)
+               return 1;
+           }
+       }
+       return 0;
+    }
+}

e0e6de87197e7fcb1169e26768416678d82143a3
DhirajHDesale <dhirajhanumantpatil@gmail.com>
2023-10-25 15:18:09 +0530

    Arithmetic Number

---
+++ Day 17/ArithmeticNumber.java
@@ -0,0 +1,15 @@
+class Solution{
+    static int inSequence(int A, int B, int C){
+       if (C == 0) {
+            
+            return (A == B) ? 1 : 0;
+        } else {
+            
+            if ((B - A) % C == 0 && (B - A) / C >= 0) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+}

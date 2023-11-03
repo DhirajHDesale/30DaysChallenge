f5b1af6927e340382be2bd7ab01db2fde70ac956
DhirajHDesale <dhirajhanumantpatil@gmail.com>
2023-11-03 13:55:29 +0530

    Wave Array

---
+++ Day 26/Wave Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static void convertToWave(int n, int[] a) {
+       
+         for (int i = 1; i < n; i += 2) {
+        int temp = a[i];
+        a[i] = a[i - 1];
+        a[i - 1] = temp;
+    }
+    }
+}
+        
